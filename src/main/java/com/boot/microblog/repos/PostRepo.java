30b6420733a0585bbded13c4b70ffe66d9839dd3
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-15 20:46:48 +0300

    added findByTag method

--- src/main/java/com/boot/microblog/repos/PostRepo.java
+++ src/main/java/com/boot/microblog/repos/PostRepo.java
@@ -3,5 +3,8 @@ package com.boot.microblog.repos;
 import com.boot.microblog.domain.PostEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepo extends CrudRepository<PostEntity, Long> {
+    List<PostEntity> findByTag(String tag);
 }
