fc0c74d5138e2e565f840b5f3ebc16c246b0e32e
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-21 22:18:45 +0300

    controller adapted for thymeleaf

--- src/main/java/com/boot/microblog/controllers/PostController.java
+++ src/main/java/com/boot/microblog/controllers/PostController.java
@@ -4,11 +4,11 @@ import com.boot.microblog.domain.PostEntity;
 import com.boot.microblog.repos.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @Controller
 public class PostController {
@@ -16,24 +16,24 @@ public class PostController {
     PostRepo postRepo;
 
     @GetMapping("/posts")
-    public String allPosts(Map<String, Object> model) {
-        Iterable<PostEntity> posts = postRepo.findAll();
-        model.put("posts", posts);
+    public String showPosts(Model model) {
+        Iterable<PostEntity> allPosts = postRepo.findAll();
+        model.addAttribute("allPosts", allPosts);
         return "posts";
     }
 
     @PostMapping("/posts")
-    public String addPost(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+    public String addPost(@RequestParam String text, @RequestParam String tag, Model model) {
         PostEntity post = new PostEntity(text, tag);
         postRepo.save(post);
 
         Iterable<PostEntity> posts = postRepo.findAll();
-        model.put("posts", posts);
+        model.addAttribute("posts", posts);
         return "posts";
     }
 
     @PostMapping("/filter")
-    public String filterPosts(@RequestParam String filter, Map<String, Object> model) {
+    public String filterPosts(@RequestParam String filter, Model model) {
         Iterable<PostEntity> posts;
         if (filter != null && !filter.isEmpty()) {
             posts = postRepo.findByTag(filter);
@@ -41,7 +41,7 @@ public class PostController {
             posts = postRepo.findAll();
         }
 
-        model.put("posts", posts);
+        model.addAttribute("posts", posts);
         return "posts";
     }
 }
