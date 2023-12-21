23c34ebf19f945ee42a8015509b408239fc0624e
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-21 22:20:54 +0300

    minor fixes

--- src/main/java/com/boot/microblog/controllers/GreetingController.java
+++ src/main/java/com/boot/microblog/controllers/GreetingController.java
@@ -2,14 +2,11 @@ package com.boot.microblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @Controller
 public class GreetingController {
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting() {
         return "greeting";
     }
 }
