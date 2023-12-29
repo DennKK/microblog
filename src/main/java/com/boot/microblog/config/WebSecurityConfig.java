528af35d269bc2ed2e7d70e73aceeaa0ed8ca321
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-29 20:58:33 +0300

    deleted password encoder and in memory auth

--- src/main/java/com/boot/microblog/config/WebSecurityConfig.java
+++ src/main/java/com/boot/microblog/config/WebSecurityConfig.java
@@ -4,21 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -33,20 +23,5 @@ public class WebSecurityConfig {
                 .logout((logout) -> logout.permitAll());
 
         return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user1 = User.builder()
-                .username("user1")
-                .password(encoder().encode("123456"))
-                .roles("USER")
-                .build();
-        UserDetails user2 = User.builder()
-                .username("user2")
-                .password(encoder().encode("123456"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
