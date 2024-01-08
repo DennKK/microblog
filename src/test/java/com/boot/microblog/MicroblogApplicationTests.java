d2ae2b5d6700ad2d0ef6fa06b6b491fdaa43dcb9
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-08 18:06:07 +0300

    improved code style

--- src/test/java/com/boot/microblog/MicroblogApplicationTests.java
+++ src/test/java/com/boot/microblog/MicroblogApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MicroblogApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
