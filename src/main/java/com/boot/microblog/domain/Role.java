cdf43355ccbbe8947424ed0f434316a221e2d603
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-10 20:09:46 +0300

    added admin role

--- src/main/java/com/boot/microblog/domain/Role.java
+++ src/main/java/com/boot/microblog/domain/Role.java
@@ -3,7 +3,7 @@ package com.boot.microblog.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
