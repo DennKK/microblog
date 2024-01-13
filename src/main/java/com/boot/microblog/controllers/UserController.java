952e4205609b3a5926cd118d86eef004c8c5b530
Denis Karpov <karpovdenis.al@gmail.com>
2024-01-13 18:18:41 +0300

    added a method for displaying the user editing form and a method for saving the updated user

--- src/main/java/com/boot/microblog/controllers/UserController.java
+++ src/main/java/com/boot/microblog/controllers/UserController.java
@@ -1,13 +1,25 @@
 package com.boot.microblog.controllers;
 
+import com.boot.microblog.domain.Role;
+import com.boot.microblog.domain.UserEntity;
 import com.boot.microblog.repos.UserRepo;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     private final UserRepo userRepo;
 
@@ -19,5 +31,46 @@ public class UserController {
     public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
         return "userList";
+    }
+
+    @GetMapping("/{userId}")
+    public String userEditForm(@PathVariable Long userId, Model model) {
+        UserEntity user = userRepo.findById(userId).orElse(null);
+        if (user == null) {
+            return "redirect:/user";
+        }
+
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") Long userId
+    ) {
+        UserEntity user = userRepo.findById(userId).orElse(null);
+        if (user == null) {
+            return "redirect:/user";
+        }
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+
+        return "redirect:/user";
     }
 }
